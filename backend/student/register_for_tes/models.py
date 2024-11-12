b3b0a2cb09af11776f819ca2bd37de1526f889ae
Shahzodbek <123619551+shahzodbeeck@users.noreply.github.com>
2024-11-12 18:56:18 +0500

    Update models.py

--- backend/student/register_for_tes/models.py
+++ backend/student/register_for_tes/models.py
@@ -1,33 +1,10 @@
-from sqlalchemy import event
-
 from app import db
 
 
-class Region(db.Model):
+class Defenation(db.Model):
+    __tablename__ = 'defenation'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-
-    def __str__(self):
-        return self.name
-
-
-class University(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    region_id = db.Column(db.Integer, db.ForeignKey('region.id'), nullable=False)
-    region = db.relationship('Region', backref=db.backref('universities', lazy=True))
-    university_id = db.Column(db.Integer, nullable=False)
-
-    def __str__(self):
-        return self.name
-
-
-class Faculty(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    university_id = db.Column(db.Integer, db.ForeignKey('university.id'), nullable=False)
-    university = db.relationship('University', backref=db.backref('faculties', lazy=True))
-    mvdir = db.Column(db.String(100))
 
     def __str__(self):
         return self.name
@@ -49,14 +26,11 @@ class StudentTestBlock(db.Model):
     father_name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(100), nullable=False)
     school_id = db.Column(db.Integer, db.ForeignKey('school.id'), nullable=False)
-    university_id = db.Column(db.Integer, db.ForeignKey('university.id'), nullable=False)
-    faculty_id = db.Column(db.Integer, db.ForeignKey('faculty.id'), nullable=False)
+    defenation_id = db.Column(db.Integer, db.ForeignKey('defenation.id'), nullable=False)
     unique_id = db.Column(db.String(300), unique=True, nullable=True)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    language = db.Column(db.String(100), nullable=False)
 
     school = db.relationship('School')
-    university = db.relationship('University')
-    faculty = db.relationship('Faculty')
-
-
-
+    defenation = db.relationship('Defenation')
 
