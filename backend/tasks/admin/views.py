c001d66e309ec636b3977f8c94b3ea54a784f279
shahzodbeeck <shahzodomonboyev1.com>
2025-06-26 17:20:20 +0500

    sdfsdf

--- backend/tasks/admin/views.py
+++ backend/tasks/admin/views.py
@@ -1,4 +1,8 @@
 from .debts import *
 from .new_students import *
 from .leads import *
-from .tasks_rating import *
+from .tasks_rating import task_rating_bp
+
+
+def register_task_rating_views(api,app):
+    app.register_blueprint(task_rating_bp, url_prefix=f"/{api}task_rating")
