576511b20045a811c14f1e23ddee6114bd33be2e
Begzod10 <rimefara22@gmail.com>
2026-01-16 16:30:49 +0500

    commit

--- backend/account/overall_datas/home_screen.py
+++ backend/account/overall_datas/home_screen.py
@@ -351,14 +351,15 @@ def home_screen_salaries():
                 StaffSalary.calendar_year == year_id,
                 Users.location_id == location_id,
                 # Staff.deleted == False,
-                # or_(
-                #     Staff.deleted == False,  # Staff not deleted
-                #     and_(
-                #         Staff.deleted == True,
-                #         CalendarMonth.date > month_date_obj
-                #         # Deleted on/after the requested month
-                #     )
-                # )
+                or_(
+                    Staff.deleted == False,  # Staff not deleted
+                    Staff.deleted == None,
+                    and_(
+                        Staff.deleted == True,
+                        CalendarMonth.date > month_date_obj
+                        # Deleted on/after the requested month
+                    )
+                )
             )
         )
 
