d25b5bb7e9360d21d38089fd1df0e23c0c6c8996
thesobirov <shahzodsobirjonov9@gmail.com>
2026-01-22 15:00:08 +0500

    afafa

--- backend/mobile/views.py
+++ backend/mobile/views.py
@@ -1,5 +1,7 @@
 from .parent import parent_mobile_bp
+from .missions.missions import missions_bp
 
 
 def register_parent_mobile_views(api, app):
     app.register_blueprint(parent_mobile_bp, url_prefix=f"/{api}/mobile")
+    app.register_blueprint(missions_bp, url_prefix=f"/{api}/mobile")
